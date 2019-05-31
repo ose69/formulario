91cce4146d8ff6053eef23aa43bb6777a34d691c
ose69 <47892767+ose69@users.noreply.github.com>
2019-05-31 09:35:50 -0500

    php
    php código funcional

--- enviar.php
+++ enviar.php
@@ -5,17 +5,27 @@ $correo=$_POST["correo"];
 $telefono=$_POST["telefono"];
 $mensaje=$_POST["mensaje"];
 //$contenido="Nombre:".$nombre."\ncorreo:".$correo."\nteléfono:".$telefono."\nMensaje:".$mensaje;
-mail($destino,"contacto",$contenido);
-header("location:loggin.html");
+/*mail($destino,"contacto",$contenido);
+header("location:loggin.html");*/
 
 ?>
-
+<!DOCTYPE html>
 <html>
-    <body>
-        <p>kjh daskjh askjh askjdh akdah </p>
-        <p> a nomber de :<?php echo $nombre;?></p>
-        <div><p> alkj dlkaj sdlkaj d<?php echo $_POST['telefono'];?></p>
-        <?php 
-
-    </body>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <form>
+        <h2>contacto</h2>
+        <div>tu nommbre es:<p><?php echo $nombre ?></p></div>
+        <div>correo:<p><?php echo $correo ?></p></div>
+        <div>teléfono:<p><?php echo $telefono ?></p></div>
+        <div>mensaje:<p> <?php echo $mensaje ?></p></div>
+    </form>
+</body>
 </html>
