a374886bafa9c42464a176dc4fd654080c02944b
ose69 <47892767+ose69@users.noreply.github.com>
2019-05-31 13:51:46 -0500

    insert
    php mysql

--- insert.php
+++ insert.php
@@ -1,10 +1,10 @@
 <?php 
-$db="id9787806_conexion";
-require_once "conexion.php"
+require_once "conexion.php";
 
-$sql="INSERT INTO nose(nombre, correo, teléfono)
-VALUES ('jorge','joochoa15@ucol.mx','31231234531')";
-if($conn->query($sql)===TRUE){
+$sql="INSERT INTO nose(nombre, correo)
+VALUES ('deniz','denizillo15@ucol.mx')";
+
+if($conn->query($sql) === TRUE){
     echo "new record created successfully";
 } else{
     echo "error:".$sql."<br>".$conn->error;
